cb02c1d6218acc28901dbb12bece92b6588ff4e4
Jeremy LeCroy <jeremy212@gmail.com>
2019-07-12 00:06:59 -0400

    rename bashrc bash

---
+++ manpage_colors.bash
@@ -0,0 +1,10 @@
+#color man pages (really the output of less)
+export LESS_TERMCAP_mb=$'\E[00;35m' # enter blinking mode = Purple
+export LESS_TERMCAP_md=$'\E[00;36m' # enter double-bright mode = Cyan
+export LESS_TERMCAP_me=$'\E[0m'     # turn off all appearance modes (mb, md, so, us)
+
+export LESS_TERMCAP_so=$'\E[00;31m' # enter standout mode = Red
+export LESS_TERMCAP_se=$'\E[0m'     # leave standout mode
+
+export LESS_TERMCAP_us=$'\E[0;32m'  # enter underline mode = Green
+export LESS_TERMCAP_ue=$'\E[0m'     # leave underline mode
